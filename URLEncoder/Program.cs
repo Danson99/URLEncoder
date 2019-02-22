3d83f76f35e55214726caebb375be70175ad3b0a
Daniel Vang <daniel@danielvang.com>
2019-02-22 09:52:07 -0600

    First commit

---
+++ URLEncoder/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URLEncoder
+{
+    class Program
+    {
+        static bool flag = false;
+        static void Main(string[] args)
+        {
+            do
+            {
+                string project_name = " ";
+                string activity_name = " ";
+
+                try
+                {
+                    Console.WriteLine("Enter the project name");
+                    project_name = Console.ReadLine();
+
+                    Console.WriteLine("Enter the project name");
+                    activity_name = Console.ReadLine();
+
+                    string url = "https://companyserver.com/content/" + project_name + "/files/" + activity_name + "/" + activity_name + "Report.pdf";
+                    Console.WriteLine("\n");
+                    Console.WriteLine(url);
+                    Console.ReadLine();
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter valid details");
+                }
+                Console.WriteLine("\n");
+                Console.WriteLine("Do you want another URL say y/n");
+                char res = Convert.ToChar(Console.ReadLine());
+                if (res == 'y')
+                {
+                    flag = true;
+                }
+            } while (flag == true);
+        }
+    }
+}
